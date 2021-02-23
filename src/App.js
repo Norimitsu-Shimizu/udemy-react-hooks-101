61afdbc00e0eee8d073b5756366db4146d18697e
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-02-24 00:16:22 +0900

    add defaultprops

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const initialStates = {
-    name: '',
-    price: 1000
-  }
-
-  const [name, setName] = useState(initialStates.name)
-  const [price, setPrice] = useState(initialStates.price)
-
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setPrice(props.price)}>Reset</button>
     </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
