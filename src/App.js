1a9b61cdeef64012f91e01927de8ded018de357c
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-02-23 22:25:52 +0900

    add increment2 and decrement2 to return function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ const App = () => {
   const increment = () => setCount(count + 1)
   const decrement = () => setCount(count - 1)
 
+  const increment2 = () => setCount(preveousCount => preveousCount + 1)
+  const decrement2 = () => setCount(preveousCount => preveousCount - 1)
+
   return (
     <>
       <div>count: {count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>+1</button>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+
+      </div>
     </>
   );
 }
