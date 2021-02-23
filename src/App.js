621c75beaeb5bbdac2fb13ae99935f6114019d5e
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-02-24 00:26:32 +0900

    add reset

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ const App = props => {
   const [name, setName] = useState(props.name)
   const [price, setPrice] = useState(props.price)
 
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
       <button onClick={() => setPrice(price + 1)}>+1</button>
       <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={() => setPrice(props.price)}>Reset</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   );
 }
