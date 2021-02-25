f85f26bd90632b22f93c4250165af720d53f62c1
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-02-25 20:35:26 +0900

    create Events component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EventForm from './EventForm'
-import Event from './Event'
+import Events from './Events'
 import reducer from '../reducers'
 
 const App = () => {
@@ -10,20 +10,7 @@ const App = () => {
   return (
     <div className="container-fluid">
       <EventForm state={state} dispatch={dispatch}/>
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
-        </tbody>
-      </table>
+      <Events state={state} dispatch={dispatch}/>
     </div>
   );
 }
